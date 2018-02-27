a02e7ff21f41e421f229928084b8190b5a5a870c
Mihai <mihaicraciun96@gmail.com>
2018-02-27 15:23:03 +0200

    Created webimage - the web app

--- deepimage/settings.py
+++ deepimage/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'webimage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ webimage/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import View
+# Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'webimage/index.html',{
+            'title' : 'Home',
+        })
