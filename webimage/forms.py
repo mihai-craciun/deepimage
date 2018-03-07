f6bab0cec8fab96f1e96b2853bcffaf2c16a2134
Mihai <mihaicraciun96@gmail.com>
2018-03-07 14:03:39 +0200

    Added user registration and login forms

--- webimage/forms.py
+++ webimage/forms.py
@@ -2,14 +2,32 @@ from django import forms
 from django.core.validators import RegexValidator, EmailValidator
 # custom forms
 
+
 class RegisterForm(forms.Form):
-    username = forms.CharField(label='username', validators=[RegexValidator('^\w{5,15}$',
-    'The username can only contain alphanumeric characters and must be between 5 and 15 characters long')]);
-    email = forms.EmailField(label='email', validators=[EmailValidator('Please enter a valid e-mail')]);
-    password = forms.CharField(widget=forms.PasswordInput, label='password')
-    confirmPassword = forms.CharField(widget=forms.PasswordInput, label='confirmPassword');
+    username = forms.CharField(label='Username', validators=[RegexValidator('^\w{5,15}$',
+                                                                            'The username can only contain alphanumeric characters and must be between 5 and 15 characters long (Aplhanumeric or underscore)')], widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
+    first_name = forms.CharField(label='First Name', widget=forms.TextInput(
+        attrs={'class': 'form-control'}), required=False)
+    last_name = forms.CharField(label='Last Name', widget=forms.TextInput(
+        attrs={'class': 'form-control'}), required=False)
+    email = forms.EmailField(label='Email', validators=[EmailValidator(
+        'Please enter a valid e-mail')], widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='password', validators=[
+        RegexValidator(
+            '[a-zA-Z]+', 'The password must contain one or more letters'),
+        RegexValidator(
+            '[0-9]+', 'The password must contain one or more digits'),
+        RegexValidator(
+            '[!@#$%^&_]+', 'The password must contain at least one special symbol "!@#$%^&_"'),
+        RegexValidator('^.{5,20}$', 'The password must be 5-15 characters long')], required=True)
+    confirm_password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}), label='Password Confirmation', required=True)
+
 
 class LoginForm(forms.Form):
-    email = forms.CharField(label='email', validators=[EmailValidator('Please enter a valid e-mail')])
-    password = forms.CharField(widget=forms.PasswordInput, label='password')
-    remember = forms.BooleanField(initial=True)
+    email = forms.CharField(label='Email', validators=[EmailValidator(
+        'Please enter a valid e-mail')], widget=forms.TextInput(attrs={'class': "form-control", "type": "email"}), required=True)
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}), label='Password', required=True)
+    remember = forms.BooleanField(label='Remember Me', initial=True, widget=forms.CheckboxInput(
+        attrs={'class': 'form-check-input'}), required=False)
--- webimage/urls.py
+++ webimage/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('gallery/', views.GalleryView.as_view(), name='gallery'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('register/', views.RegisterView.as_view(), name='register')
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
