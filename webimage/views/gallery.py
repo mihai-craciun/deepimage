cc5889e138a12b1eab4870d9dbc5ba40cd69c93f
Mihai Craciun <mihaicraciun96@gmail.com>
2018-04-15 11:53:07 +0300

    added users search and users gallery

--- webimage/views/gallery.py
+++ webimage/views/gallery.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
+from ..models import User, Album, Photo, PhotoTag, Tag
+from .models.userview import user_albums_rank, user_photos_rank, user_tags_rank
 
 
 class Fields:
@@ -7,54 +9,83 @@ class Fields:
     Users = 'Users'
 
 
-class Title:
-    Tags = 'Gallery - Tags'
-    Users = 'Gallery - Users'
+class RenderObject:
+    class Title:
+        Tags = 'Gallery - Tags'
+        Users = 'Gallery - Users'
+
+    @staticmethod
+    def create(field_type, multiple, context=None):
+        obj = {}
+        if field_type == Fields.Tags:
+            obj['title'] = RenderObject.Title.Tags
+            obj['field'] = Fields.Tags
+        elif field_type == Fields.Users:
+            obj['title'] = RenderObject.Title.Users
+            obj['field'] = Fields.Users
+        if multiple:
+            obj['gallery_mode'] = True
+        else:
+            obj['gallery_mode'] = False
+        if context is not None:
+            obj.update(context)
+        return obj
 
 
 class UsersView(View):
     def get(self, request):
-        return render(request, 'webimage/gallery/users.html', {
-            'title': Title.Users,
-            'field': Fields.Users,
-        })
+        userstring = request.GET.get('userstring', None)
+        users = UsersView.filter(userstring)
+        users = list(map(lambda u: {
+            'user': u,
+            'albums': user_albums_rank(u),
+            'photos': user_photos_rank(u),
+            'tags': user_tags_rank(u),
+        }, users))
+        context = {
+            'users': users,
+            'filter': userstring,
+        }
+        return render(request, 'webimage/gallery/users.html',
+                      RenderObject.create(Fields.Users, True, context))
 
-
-class TagsView(View):
-    def get(self, request):
-        return render(request, 'webimage/gallery/tags.html', {
-            'title': Title.Tags,
-            'field': Fields.Tags,
-        })
-
-
-class TagView(View):
-    def get(self, request, tag):
-        return render(request, 'webimage/gallery/tag.html', {
-            'title': Title.Tags,
-            'field': Fields.Tags,
-        })
+    @staticmethod
+    def filter(userstring):
+        if userstring is None:
+            return User.objects.all()
+        username_search = User.objects.all().filter(username__icontains=userstring)
+        first_name_search = User.objects.all().filter(first_name__icontains=userstring)
+        last_name_search = User.objects.all().filter(last_name__icontains=userstring)
+        return username_search | first_name_search | last_name_search
 
 
 class UserView(View):
     def get(self, request, user):
-        return render(request, 'webimage/gallery/user.html', {
-            'title': Title.Users,
-            'field': Fields.Users,
-        })
+        profile = User.objects.get(username=user)
+        context = {'profile': profile}
+        return render(request, 'webimage/gallery/user.html',
+                      RenderObject.create(Fields.Users, True, context))
 
 
 class AlbumView(View):
     def get(self, request, user, album):
-        return render(request, 'webimage/gallery/album.html', {
-            'title': Title.Users,
-            'field': Fields.Users,
-        })
+        return render(request, 'webimage/gallery/album.html',
+                      RenderObject.create(Fields.Users, True))
 
 
 class PhotoView(View):
     def get(self, request, user, album, photo):
-        return render(request, 'webimage/gallery/album.html', {
-            'title': Title.Users,
-            'field': Fields.Users,
-        })
+        return render(request, 'webimage/gallery/album.html',
+                      RenderObject.create(Fields.Users, False))
+
+
+class TagsView(View):
+    def get(self, request):
+        return render(request, 'webimage/gallery/tags.html',
+                      RenderObject.create(Fields.Tags, True))
+
+
+class TagView(View):
+    def get(self, request, tag):
+        return render(request, 'webimage/gallery/tag.html',
+                      RenderObject.create(Fields.Tags, False))
