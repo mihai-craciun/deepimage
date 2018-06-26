e643fed9420007334ef6c5815b8d3882fca98e16
Mihai Craciun <mihaicraciun96@gmail.com>
2018-06-26 11:04:23 +0300

    Loading spinner + working admin page

--- webimage/static/webimage/js/main.js
+++ webimage/static/webimage/js/main.js
@@ -1,3 +1,6 @@
 setSquarePhotos = () => {$('img.card-img-top.gallery-img').height($('img.card-img-top.gallery-img').width())}
 $(window).ready(setSquarePhotos)
 $(window).resize(setSquarePhotos)
+let loadingSpinner = (button) => {
+    $(button).children().addClass('fa-spinner').addClass('fa-spin').removeClass('fa-tags')
+}
